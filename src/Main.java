5d2ac724a77f32c90a2f942630dd2de46236c112
Rinat199903 <rinat25178@gmail.com>
2023-07-28 16:42:07 +0300

    changes

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.net.InterfaceAddress;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +12,7 @@ public class Main {
         compare(5, daysInYear);
         compare(15, daysInYear);
 
+
     }
 
     public static void compare(int day, int[] daysInYear) {
@@ -23,33 +25,24 @@ public class Main {
     }
 
     public static int chooseHobbyRecursive(int[] startNumbers, int day, int[] daysInYear) {
-        System.out.println(">>> " + day);
-        List<Integer> numbers = new ArrayList<>();
-
-        numbers.add(startNumbers[0]);
-        numbers.add(startNumbers[1]);
-        numbers.add(startNumbers[2]);
-        numbers.add(startNumbers[3]);
-        if (daysInYear[day] != 0) return daysInYear[day];//возвращаем значение массива
-        int d = 0;//заводим опорную переменную
-        return countDays(numbers, day, daysInYear, d);//используем метод подсчета по формуле
-    }
-
-
-    private static int countDays(List<Integer> numbers, int day, int[] daysInYear, int d) {
-        while (day > d) {
-            int index = d + 4; // индексы дней в массиве сдвинуты на 4
-            int prev = numbers.get(index - 1); // предыдущее значение
-            int prePrePrev = numbers.get(index - 3); // пре-пре-предыдущее значение
-            numbers.add((prev * prePrePrev) % 10 + 1);
-            d++;
-            countDays(numbers, day, daysInYear, d);//используем рекурсию
-
+        if (daysInYear[day - 1] != 0) return daysInYear[day - 1];
+        if (day <= 3 && daysInYear[day] == 0) {
+            daysInYear[0] = (startNumbers[3] * startNumbers[0]) % 10 + 1;
+            daysInYear[1] = (daysInYear[0] * startNumbers[2]) % 10 + 1;
+            daysInYear[2] = (daysInYear[1] * startNumbers[3]) % 10 + 1;
+            return daysInYear[day - 1];
+        } else {
+            if (daysInYear[day - 2] != 0) {
+                int index = day - 1; // индексы дней в массиве сдвинуты на 4
+                int prev = daysInYear[index - 1]; // предыдущее значение
+                int prePrePrev = daysInYear[index - 3]; // пре-пре-предыдущее значение
+                daysInYear[day - 1] = (prev * prePrePrev) % 10 + 1;
+            } else {
+                daysInYear[day - 2] = chooseHobbyRecursive(startNumbers, day - 1, daysInYear);
+                daysInYear[day - 1] = chooseHobbyRecursive(startNumbers, day, daysInYear);
+            }
         }
-        daysInYear[day] = numbers.get(numbers.size() - 1);
-        return numbers.get(numbers.size() - 1);
-
-
+        return daysInYear[day - 1];
     }
 
     public static int chooseHobbyIterative(int[] startNumbers, int day) {
